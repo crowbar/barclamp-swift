b96a8b08022922b556ef1c98200febdff4bd5c68
Judd Maltin <judd_maltin@dell.com>
2013-06-10 18:58:33 -0400

    html to plaintext line ending

--- crowbar_framework/app/models/swift_service.rb
+++ crowbar_framework/app/models/swift_service.rb
@@ -286,7 +286,7 @@ class SwiftService < ServiceObject
     end
 
     if errors.length > 0
-      raise Chef::Exceptions::ValidationFailed.new(errors.join("<br/>"))
+      raise Chef::Exceptions::ValidationFailed.new(errors.join("\n"))
     end
   end
 
