cdadccdab84b5845fe3f593b835501b45e8f4562
Jiří Suchomel <jsuchome@suse.cz>
2014-01-17 11:07:34 +0100

    insecure flag

--- chef/data_bags/crowbar/migrate/swift/003_add_ssl_attributes.rb
+++ chef/data_bags/crowbar/migrate/swift/003_add_ssl_attributes.rb
@@ -3,6 +3,7 @@ def upgrade ta, td, a, d
   a["ssl"]["certfile"]          = ta["ssl"]["certfile"]
   a["ssl"]["keyfile"]           = ta["ssl"]["keyfile"]
   a["ssl"]["generate_certs"]    = ta["ssl"]["generate_certs"]
+  a["ssl"]["insecure"]          = ta["ssl"]["insecure"]
   return a, d
 end
 
