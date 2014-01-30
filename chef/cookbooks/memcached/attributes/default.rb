1ce01db46a08a18fbc84fcb8858c31a62eb2655b
Vincent Untz <vuntz@suse.com>
2014-01-30 14:57:22 +0100

    Use memcached user for memcached on SUSE
    https://bugzilla.novell.com/show_bug.cgi?id=861313

--- chef/cookbooks/memcached/attributes/default.rb
+++ chef/cookbooks/memcached/attributes/default.rb
@@ -1,4 +1,9 @@
 default[:memcached][:memory] = 64
 default[:memcached][:port] = 11211
-default[:memcached][:user] = "nobody"
 default[:memcached][:listen] = "0.0.0.0"
+case node[:platform]
+when "suse"
+  default[:memcached][:user] = "memcached"
+else
+  default[:memcached][:user] = "nobody"
+end
